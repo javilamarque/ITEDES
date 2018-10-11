f108fbec4dcf5fa9b3834ed23455f3d6082ae370
javier <javier@javier.hogar>
2018-10-11 14:59:18 -0300

    Se corrigio genero.py

--- modulo1/segmento3/Estructura_de_Decision_Simple/python/genero.py
+++ modulo1/segmento3/Estructura_de_Decision_Simple/python/genero.py
@@ -1,3 +1,6 @@
 genero = input("Ingrese su genero: ")
 if genero == "Femenino" or genero == "Masculino":
     print("Su sexo es: " + genero)
+
+else: 
+    print("Usted no ingreso un genero valido")
